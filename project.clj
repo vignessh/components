9f26a7c98eb0f0c8d9dd8a6a39969a06440eb3ba
Vignessh <vignessh@gmail.com>
2015-08-24 23:31:04 +0530

    For deploying to Clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
 (defproject components "0.1.0-SNAPSHOT"
   :description "Components for popular libraries"
-  :url "https://github.com/components"
+  :url "https://github.com/vignessh/components"
+  :scm {:name "git"
+        :url "https://github.com/vignessh/components"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :signing {:gpg-key "190CBD05"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :pom-addition [:developers [:developer
+                              [:name "Vignessh Vaidyanathan"]
+                              [:url "https://vignessh.github.io"]]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.2.3"]
                  [prismatic/schema "0.4.3"]
